753e14012c5d621f97d1197c356830ef2ff22b0d
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-03-23 15:39:58 +0100

    add eltype(::Type{<:Distribution})

--- src/RandomMonad.jl
+++ src/RandomMonad.jl
@@ -12,4 +12,13 @@ of produce random values of type `T` via `rand`-related functions.
 abstract type Distribution{T} end
 
 
+"""
+    Base.eltype(::Type{<:Distribution{T}}) where {T}
+
+Return the type parameter `T` of a distribution, i.e. the type of
+values drawn from it.
+"""
+Base.eltype(::Type{<:Distribution{T}}) where {T} = T
+
+
 end # module

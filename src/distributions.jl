59b6b204e50a2babe8411645c3bc3bda9f059a84
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-03-23 16:29:43 +0100

    import Normal from RandomExtensions

--- src/distributions.jl
+++ src/distributions.jl
@@ -22,3 +22,34 @@ Base.getindex(x::UniformWrap) = x.val
 
 Sampler(RNG::Type{<:AbstractRNG}, d::Union{UniformWrap,UniformType}, n::Repetition) =
     Sampler(RNG, d[], n)
+
+
+## Normal
+
+abstract type Normal{T} <: Distribution{T} end
+
+struct Normal01{T} <: Normal{T} end
+
+struct Normalμσ{T} <: Normal{T}
+    μ::T
+    σ::T
+end
+
+const NormalTypes = Union{AbstractFloat,Complex{<:AbstractFloat}}
+
+Normal(::Type{T}=Float64) where {T<:NormalTypes} = Normal01{T}()
+Normal(μ::T, σ::T) where {T<:NormalTypes} = Normalμσ(μ, σ)
+Normal(μ::T, σ::T) where {T<:Real} = Normalμσ(AbstractFloat(μ), AbstractFloat(σ))
+Normal(μ, σ) = Normal(promote(μ, σ)...)
+
+
+### sampling
+
+rand(rng::AbstractRNG, ::SamplerTrivial{Normal01{T}}) where {T<:NormalTypes} =
+    randn(rng, T)
+
+Sampler(RNG::Type{<:AbstractRNG}, d::Normalμσ{T}, n::Repetition) where {T} =
+    SamplerSimple(d, Sampler(RNG, Normal(T), n))
+
+rand(rng::AbstractRNG, sp::SamplerSimple{Normalμσ{T},<:Sampler}) where {T} =
+    sp[].μ + sp[].σ  * rand(rng, sp.data)
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,12 @@ using RandomMonad
     @test rand(Uniform(1:10)) ∈ 1:10
     @test rand(Uniform(Int)) isa Int
 end
+
+@testset "Normal" begin
+    @test rand(Normal()) isa Float64
+    @test rand(Normal(0.0, 1.0)) isa Float64
+    @test rand(Normal(0, 1)) isa Float64
+    @test rand(Normal(0, 1.0)) isa Float64
+    @test rand(Normal(Float32)) isa Float32
+    @test rand(Normal(ComplexF64)) isa ComplexF64
+end

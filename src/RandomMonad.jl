e7170f1c26e7a8c64fd4b3a6fa0f221a9935f178
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-03-24 17:28:35 +0100

    add Zip combinator

--- src/RandomMonad.jl
+++ src/RandomMonad.jl
@@ -3,7 +3,8 @@ module RandomMonad
 export Distribution
 
 export Bernoulli, Binomial, Categorical, CloseOpen, OpenClose, OpenOpen,
-       CloseClose, Exponential, Filter, Map, MixtureModel, Normal, Uniform
+       CloseClose, Exponential, Filter, Map, MixtureModel, Normal, Uniform,
+       Zip
 
 using Random: AbstractRNG, gentype, randexp, randn, Random, Repetition,
       SamplerSimple, SamplerTag, SamplerTrivial

122f993764921c743ae44bd5d14d8d421a5d9f11
Rafael Fourquet <fourquet.rafael@gmail.com>
2021-11-27 18:23:17 +0100

    add docstrings for `Str` & `AsciiStr`

--- src/strings.jl
+++ src/strings.jl
@@ -18,6 +18,29 @@ end
 
 ## Str #######################################################################
 
+# document example:  rand(Str(Keep(!isspace, Keep(isprint, Keep(isascii, Char)))))
+# or rand(Str(Keep(isprint, Char), 40), 10)
+
+"""
+    Str([chars], [len=8]) :: Distribution{String}
+
+Create a distribution generating strings of length `len`, consisting of
+characters from `chars`, which defaults to the set of upper- and lower-case
+letters and the digits 0-9. A call to `ranstring(rng, x...)` is equivalent
+to `rand(rng, Str(x...))`. `chars` can also be `Char`, to generate any
+unicode characters, or any distribution generating characters.
+
+# Examples
+```julia-repl
+julia> rand(Str())
+"GHTYhgfT"
+
+julia> rand(Str(Keep(isprint, Char)))
+"쳳𩌸篍𝞁𩚦ḏ𐮆ꌲ"
+```
+
+See also [`AsciiStr`](@ref).
+"""
 struct Str{C} <: AbstractStr
     len::Int
     chars::C
@@ -40,6 +63,30 @@ end
 
 ## AsciiStr ##################################################################
 
+"""
+    AsciiStr([chars], [len=8]; check::Bool=true) :: Distribution{String}
+
+Similar to `Str(chars, len)` except that only ASCII strings are produced.
+This means that `chars` must specify only ASCII characters, which is checked
+for if `check` is `true`. When `chars` is not specified, it defaults to the
+set of printable ASCII characters (cf. `isprint`).
+
+Unlike for `Str`, `chars` cannot be a distribution, and must be convertible
+to `Vector{UInt8}`.
+
+One benefit of `AsciiStr` over [`Str`](@ref) is efficiency, as the size of all
+characters is known in advance. `AsciiStr(chars; check=false)` should have the
+same performance `Str(collect(UInt8, chars)`.
+
+# Example
+```julia-repl
+julia> rand(AsciiStr(20)) # only printable characters
+"gw2.}pqF_r\\"/w- x?]2r"
+
+julia> rand(AsciiStr(Char)) # any ASCII character
+"tV\\x0eeb'\\b\\x04"
+```
+"""
 struct AsciiStr <: AbstractStr
     len::Int
     chars::Vector{UInt8}
